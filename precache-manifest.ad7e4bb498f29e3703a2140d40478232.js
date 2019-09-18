a2e2f8c23beb45153d625e22f501e4c2df13efc8
sandyboypraper <sandyboyraper@gmail.com>
2019-09-19 01:17:37 +0530

    Updates

---
+++ precache-manifest.ad7e4bb498f29e3703a2140d40478232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90059db9e7a717fab3f14c3251b19b06",
+    "url": "/BMS_NS/index.html"
+  },
+  {
+    "revision": "78c202ba18193beb83a8",
+    "url": "/BMS_NS/static/css/main.396180ff.chunk.css"
+  },
+  {
+    "revision": "426d45a86ac192a1f591",
+    "url": "/BMS_NS/static/js/2.05190124.chunk.js"
+  },
+  {
+    "revision": "78c202ba18193beb83a8",
+    "url": "/BMS_NS/static/js/main.27949e49.chunk.js"
+  },
+  {
+    "revision": "ed96dcd81c893bcffc64",
+    "url": "/BMS_NS/static/js/runtime~main.4b562905.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BMS_NS/static/media/logo.5d5d9eef.svg"
+  }
+]);
